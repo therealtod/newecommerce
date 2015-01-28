d6ab29f78ea523882a6e03789e71cbf7f175c427
tod <tod@tod-PC>
2015-01-28 17:13:18 +0100

    Merge origin/master
    Conflicts:
    	storescripts/crea_tabella_metodo_pagamento.php

--- user_creditcard.php
+++ user_creditcard.php
@@ -1,89 +1,71 @@
 <?php
 session_start();
 include "./storescripts/connect_to_mysql.php";
-           if (!isset($_SESSION["userid"]))
-    {
-        header("location: login.php"); 
-    }
-    else {
-        $id= $_SESSION["userid"];
-        
-    }
-   ?>
-<?php
-/* Cominciamo ad elaborare i dati del form solo se l'utente ha riempito entrambi
- * i campi.
- */
-if(isset($_POST["creditcardnew"])){
-    $creditcard = preg_replace('#[^A-Za-z0-9]#i', '', $_POST["creditcardnew"]);
-    $query = mysql_query("INSERT INTO creditcard ( userid, cardnum )
-            VALUES ('$id', '$creditcard'") or die("Err:" . mysql_error());   
-
-}
-
-if(isset($_POST["creditcard"])){
-    $creditcard = preg_replace('#[^A-Za-z0-9]#i', '', $_POST["creditcard"]);
-    $query = mysql_query("UPDATE creditcard SET card_num='$creditcard' WHERE userid='$id'") or die("Err:" . mysql_error());   
-
-}
-    
-
+require ('./user_verify_script.php');
 ?>
-    <?php
-    $credit_card ="";
-$sql = mysql_query("SELECT * FROM creditcard WHERE userid=$id LIMIT 1");
-    $userCount = mysql_num_rows($sql);
-    if ($userCount > 0) {
-     $row = mysql_fetch_array($sql);
-            $creditcard = $row["creditcard"];
-            $credit_card .= "Attualmente hai associato la seguente carta al tuo utente: <b>$creditcard</b><br><br>"
-                    ."vuoi aggiornarla? Inseriscine una nuova: <input name='creditcard' type='text' id='creditcard' size='64' />";
-           
+
+<?php
+$id = $_SESSION["userid"];
+$sql = mysql_query("SELECT * FROM creditcard WHERE userid=$id LIMIT 1") or die (mysql_error());
+$rowcount = mysql_num_rows($sql);
+if ($rowcount > 0) {
+    $row = mysql_fetch_array($sql);
+    $creditcard = $row["cardnum"];
+    $message = "Attualmente hai associato la seguente carta al tuo account: <b>$creditcard</b><br>"
+            . "Per sostituirla, scrivi qui in basso il numero della nuova carta e conferma<br><br>";
+} else {
+    $message = "Non vi sono carte di credito associate al tuo utente, inseriscine una:";
+}
+?>
+<?php
+if (isset($_POST["creditcard"])) {
+    $creditcard = preg_replace('#[^A-Za-z0-9]#i', '', $_POST["creditcard"]);
+    if ($rowcount == 0) {
+        $query = mysql_query("INSERT INTO creditcard ( userid, cardnum )
+            VALUES ($id, '$creditcard')") or die("Err:" . mysql_error());
+    } else {
+        $query = mysql_query("UPDATE creditcard SET cardnum='$creditcard' WHERE userid=$id") or die("Err:" . mysql_error());
+        header ("location: user_creditcard.php");
     }
-        else {
-            $credit_card .= "Non vi sono carte di credito associate al tuo utente, inseriscine una: "
-                    . "<input name='creditcardnew' type='text' id='creditcard' size='64' />";
-                
-        }         
- 
-         
-    
+}
 ?>
 <!DOCTYPE html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Pannello Utente - <?php echo $_SESSION["username"]; ?>  - NewEcommerce</title>
-        <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
-    </head>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Pannello Utente - <?php echo $_SESSION["username"]; ?>  - NewEcommerce</title>
+    <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+</head>
 
-    <body>
-        <div align="center" id="mainWrapper">
-            <?php include_once("template_header.php"); ?>
-            <div id="pageContent"><br />
-                
-    
-                <form action="user_creditcard.php" enctype="multipart/form-data" name="myForm" id="myform" method="post">
-                    <table width="90%" border="0" cellspacing="0" cellpadding="6">
-                        
-                        <tr>
-                            <?php echo $credit_card; ?>
-                        </tr>
-                      
-                        
-                        
-                            <td><label>
-                                   
-                                    <input type="submit" name="button" id="button" value="Conferma Modifiche" />
-                                </label></td>
-                        </tr>
-                    </table>
-                </form>
-                <p>
-                    <a href="user_panel.php">Torna al tuo Pannello utente</a></p>
+<body>
+    <div align="center" id="mainWrapper">
+<?php include_once("template_header.php"); ?>
+        <div id="pageContent"><br />
 
-                </div>
-                                           <?php include_once("template_footer.php");             ?>
-                
-            </div>
-    </body>
+
+            <form action="user_creditcard.php" enctype="multipart/form-data" name="myForm" id="myform" method="post">
+                <table width="90%" border="0" cellspacing="0" cellpadding="6">
+
+                    <tr>
+<?php echo $message; ?>
+
+                    <input name='creditcard' type='text' id='creditcard' size='64' />
+                    </tr>
+
+
+
+                    <td><label>
+
+                            <input type="submit" name="button" id="button" value="Conferma Modifiche" />
+                        </label></td>
+                    </tr>
+                </table>
+            </form>
+            <p>
+                <a href="user_panel.php">Torna al tuo Pannello utente</a></p>
+
+        </div>
+<?php include_once("template_footer.php"); ?>
+
+    </div>
+</body>
 </html>
