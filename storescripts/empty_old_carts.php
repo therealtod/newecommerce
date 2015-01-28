d7ab99dc710453b9bb989d3e21b7e80b4939ab16
tod <tod@tod-PC>
2015-01-28 22:44:46 +0100

    Merge origin/master
    Conflicts:
    	storescripts/crea_tabella_metodo_pagamento.php

---
+++ storescripts/empty_old_carts.php
@@ -0,0 +1,16 @@
+<?php
+require ('./connect_to_mysql.php');
+?>
+<?php
+$query = mysql_query("SELECT id, last_cart_mod_date FROM utente");
+while ($row = mysql_fetch_array($query))
+{
+    $userid = $row["id"];
+    $cart_date =  strtotime($row["last_cart_mod_date"]);
+    if ($cart_date < strtotime (strtotime ('1 week ago', strtotime('now'))))
+      {
+          $query = mysql_query ("DELETE FROM carrello WHERE user_id= $userid");
+      }
+}
+?>
+
